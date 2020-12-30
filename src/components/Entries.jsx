aa2cc1d0e0bdf743ec595d404364d53dbe97c827
Erica <ericasu33@gmail.com>
2020-12-29 22:15:32 -0800

    display current time and display current month / date according to user selection, default on browser render is today.

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 
 
 function Entries() {
-  const [desc, setDesc] = useState("");
+  const [desc, setDesc] = useState('');
   const [toggleCalendar, setToggleCalendar] = useState(false);
   const [calendarValue, setCalendarValue] = useState(new Date());
 
@@ -13,32 +13,48 @@ function Entries() {
     setToggleCalendar(prev => !prev)
   }
 
+  // https://stackoverflow.com/questions/10599148/how-do-i-get-the-current-time-only-in-javascript
+  const getTimeNow = new Date().toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+
   return (
     <div>
       <form
-        autoComplete="off"
+        autoComplete='off'
         onSubmit={event => event.preventDefault()}
       >
+
+        {/* <EntryDesc />  */}
         <div>
           <input
             value={desc}
             onChange={(event) => setDesc(event.target.value)}
-            name="desc"
-            type="text"
-            placeholder="What are you working on?"
-            size="50"
+            name='desc'
+            type='text'
+            placeholder='What are you working on?'
+            size='50'
           />
 
           {console.log(desc)}
         </div>
+
         {/* <StartEndTime />  */}
+        <div>
+            {getTimeNow}
+        </div>
         {/* no manual input yet */}
         {/* <Intensity /> */}
 
         <i 
-          className="fa fa-calendar-alt fa-lg"
+          className='fa fa-calendar-alt fa-lg'
           onClick={(e) => setToggleCalendar(prev => !prev)}
-        />
+        >
+          {calendarValue.getMonth() + 1}/{calendarValue.getDate()}
+        </i>
+      
  
         {toggleCalendar && 
         <Calendar 
@@ -48,7 +64,7 @@ function Entries() {
         /> 
         }
 
-        {console.log(calendarValue)}
+        {console.log({calendarValue})}
 
         {/* https://www.npmjs.com/package/react-calendar */}
         {/* <TimerDuration /> */}
