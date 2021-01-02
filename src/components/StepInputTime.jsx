39d19b3c7c1272424231f44f8cfad56e2db2bce0
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-01 17:25:50 -0800

    WIP get StepInputTime changing by setMinutes, getMinutes etc.

--- src/components/StepInputTime.jsx
+++ src/components/StepInputTime.jsx
@@ -5,6 +5,7 @@ import './StepInput.scss'
 /* TODO:
   * Conditionally render the chevrons
   * Make this component universal? Or make one component for integers, one for clocks/timers
+  * Distinguish between clock time and timer time: UTC or not?
 */
 
 const StepInputTime = function(props) {
@@ -18,36 +19,51 @@ const StepInputTime = function(props) {
   console.log('timeStr:', timeStr);
 
   const updateFromInputStr = function(inputStr) {
+    // TODO: extract hours:minutes:seconds from display time based on props.display
+    const displayKeys = Object.keys(props.display)
+    console.log('...displayKeys:', ...displayKeys);
     const [h, m, s] = inputStr.split(':')
     console.log('h:m:s', h, ':', m, ':', s);
+    
     setValue(prev => {
-      const newTime = prev
-      if (h) prev.setHours = h
-      if (m) prev.setMinutes = m
-      if (s) prev.setSeconds = s
+      const newTime = new Date(prev)
+      if (h) newTime.setHours(h)
+      if (m) newTime.setMinutes(m)
+      if (s) newTime.setSeconds(s)
       return newTime
     })
   }
 
-  const stepValue = function (value, sign, stepSize = props.stepSize || '1:00') {
+  const updateByStepValue = function (sign, stepSize = props.stepSize || '1:00') {
     const [m, s] = stepSize.split(':')
-    const stepSizeMillis = m * 60 * 1000 + s * 1000
-    return new Date(Number(value) + sign * Number(stepSizeMillis))
+    // console.log('m:s', ':', m, ':', s);
+
+    // const stepSizeMillis = m * 60 * 1000 + s * 1000
+    return setValue(prev => {
+      const newTime = new Date(prev)
+      // console.log('prev.getMinutes:', prev.getMinutes());
+      // console.log('prev.getSeconds:', prev.getSeconds());
+      newTime.setMinutes(prev.getMinutes() + m * sign)
+      newTime.setSeconds(prev.getSeconds() + s * sign)
+      // const newTime = newTime
+      console.log(newTime);
+
+      return newTime
+    })
+    // return new Date(Number(value) + sign * Number(stepSizeMillis))
   }
 
   return (
     <>
-      <i className="fa fa-chevron-up" onClick={e => setValue(prev => stepValue(prev, 1))}></i>
+      <i className="fa fa-chevron-up" onClick={e => updateByStepValue(1)}></i>
       <input
-        // value={`${h}:${m}`}
         value={timeStr}
-        // onChange={e => setValue(e.target.value)}
         onChange={e => updateFromInputStr(e.target.value)}
-        // onChange={e => console.log('--- input value is:', typeof e.target.value)}
+        // onChange={e => console.log('--- input value is:', e.target.value)}
         name={props.name}
         type='text'
       />
-      <i className="fa fa-chevron-down" onClick={e => setValue(prev => stepValue(prev, -1))}></i>
+      <i className="fa fa-chevron-down" onClick={e => updateByStepValue(-1)}></i>
     </>
   )
 }
