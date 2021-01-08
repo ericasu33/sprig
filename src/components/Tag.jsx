ff5bbe189377d3607f72ad4e68daf69c4d934ac3
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-08 12:05:46 -0800

    Remove color property from Tag component

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -3,12 +3,9 @@ import classNames from 'classnames';
 import './Tag.scss';
 
 const Tag = (props) => {
-  const style = {
-    backgroundColor: props.color,
-  };
   let tagClass = classNames("tag");
   return (
-    <div className={tagClass} style={style}>
+    <div className={tagClass}>
       {props.name}
     </div>
   );
