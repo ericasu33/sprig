3e32414a96fe973253a5cb5955d04ad6fcb5704c
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-16 15:47:44 -0800

    refactor, remove comments
    - change 'toggle' state object to 2 individual states to simplify getting/setting these bools

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -14,11 +14,20 @@ interface Data {
 
 const data: Data = {};
 
-const Stopwatch = () => {
-  // const data = [1, 1, 'final-planning', '2020-12-30 00:18:02+00', '2020-12-30 20:05:23+00', 200, 1];
+/* TODO
+- Render calendar as a layer on top of the rest of the page, so it doesn't move other elements around
+- Link Start time in functions to time passed to stepInputClock
+- On date change, update y, m, d of start-time in state and db
+- Debug play/pause, sometimes it renders as a weird time with letters?
+- Change 'stop' to 'save', save the data and reset the component
+DONE Set clock start time to have max time of .now()
+*/
 
-  const [desc, setDesc] = useState('');
-  const [calendarValue, setCalendarValue] = useState(new Date());
+const Stopwatch = () => {
+  const [description, setDescription] = useState('');
+  const [calendarDate, setCalendarDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false)
+  const [isActiveTimer, setIsActiveTimer] = useState(false)
   const [initTimer, setInitTimer] = useState({
     start: null,
     pause: null,
@@ -26,36 +35,18 @@ const Stopwatch = () => {
     action: null,
   });
 
-  const [toggle, setToggle] = useState({
-    calendar: false,
-    play: false,
-  })
-
-  const calendarState = (value: any) => {
-    setCalendarValue(value)
-    setToggle(prev => {
-      return {
-        ...prev,
-        calendar: !prev.calendar,
-      };
-    })
+  const calendarState = (value: Date) => {
+    console.log(value);
+    
+    setCalendarDate(value)
+    setShowCalendar(!showCalendar)
   }
 
   const timerAction = (timephase: string) => {
-    setToggle(prev => {
-      if (timephase === 'stop') {
-        return {
-          ...prev,
-          play: false,
-        };
-      }
-
-      return {
-        ...prev,
-        play: !prev.play,
-      };
+    setIsActiveTimer(() => {
+      if (timephase === 'stop') return false
+      return !isActiveTimer;
     })
-
 
     setInitTimer( (prev: any) => {
       if (timephase !== 'start' || !data[timephase]) data[timephase] = new Date();
@@ -66,17 +57,7 @@ const Stopwatch = () => {
         action: timephase,
       };
     });
-  } 
-
-  // https://stackoverflow.com/questions/10599148/how-do-i-get-the-current-time-only-in-javascript
-  const getLocalTimeNow = new Date().toLocaleTimeString([], {
-    hour: '2-digit',
-    minute: '2-digit',
-  });
-  // const getLocalTimeNow = new Date()
-  // const test = {a: 1, b:2}
-  // const test2 = ['a', 'b']
-
+  }
 
   return (
     <>
@@ -92,11 +73,11 @@ const Stopwatch = () => {
         onSubmit={event => event.preventDefault()}
       />
 
-        {/* <EntryDesc />  */}
+        {/* <Task description />  */}
         <div>
           <input
-            value={desc}
-            onChange={(event) => setDesc(event.target.value)}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
             name='desc'
             type='text'
             form='form1'
@@ -111,9 +92,9 @@ const Stopwatch = () => {
             name='startTime'
             display={{hour: '2-digit', minute: '2-digit'}}
             value={new Date()}
+            allowFuture='false'
           />
         </div>
-        {/* no manual input yet */}
 
         {/* <Intensity /> */}
         <span className='intensityPadding'>
@@ -127,65 +108,36 @@ const Stopwatch = () => {
           %
         </span>
      
-        <Button 
-          onClick={(e: any) => setToggle(prev => {
-            return {
-              ...prev, 
-              calendar: !prev.calendar
-            };
-          })}
-        >
-        <i className='fa fa-calendar-alt fa-lg'>
-          {calendarValue.getMonth() + 1}/{calendarValue.getDate()}
-        </i>
+        <Button onClick={() => setShowCalendar(!showCalendar)}>
+          <i className='fa fa-calendar-alt fa-lg'></i>
         </Button>
-      
  
-        {toggle.calendar && 
-        <Calendar 
-          value = {calendarValue}
-          onClickDay={(value: any, event: any) => calendarState(value)}
-
-        /> 
+        {showCalendar && 
+          <Calendar 
+            value = {calendarDate}
+            onClickDay={(value: Date) => calendarState(value)}
+          /> 
         }
 
         <Timer 
           startTime = {initTimer}
         />
 
-        {!toggle.play &&
-          <Button
-            play
-            onClick={(e: any) => timerAction("start")}
-          >
+        {!isActiveTimer &&
+          <Button play onClick={(e: any) => timerAction("start")}>
             <i className="far fa-play-circle fa-lg"></i>
           </Button>
         }
 
-
-        {toggle.play &&
-          <Button
-          pause
-          onClick={(e: any) => timerAction("pause")}
-          >
+        {isActiveTimer &&
+          <Button pause onClick={(e: any) => timerAction("pause")}>
             <i className="far fa-pause-circle fa-lg"></i>
           </Button>
-                 }
+        }
 
-
-        <Button
-          stop
-          onClick={(e: any) => timerAction("stop")}
-        >
-          <i className="far fa-stop-circle fa-lg"></i>
+        <Button stop onClick={(e: any) => timerAction("stop")}>
+          <i className="far fa-save fa-lg"></i>
         </Button>
-
-        {/* https://www.npmjs.com/package/react-calendar */}
-        {/* <TimerDuration /> */}
-        {/* <Button /> */}
-        {/* Pass in img as prop & conditionals to render the component diff */}
-        {/* Play/Pause/Stop/Duplicate/Delete */}
-        {/* <ModeToggle /> STRETCH */}
         
         <Category />
     </>
