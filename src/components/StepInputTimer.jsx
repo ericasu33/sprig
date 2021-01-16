76dd8fd1b540857395aee7fa922bb63c0021917c
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-15 16:11:57 -0800

    refactor and clean up function roles so they are clearer
    missing min/max check for click adjustments

--- src/components/StepInputTimer.jsx
+++ src/components/StepInputTimer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import './StepInput.scss'
 
@@ -6,56 +6,78 @@ const StepInputTimer = function(props) {
   const [value, setValue] = useState(props.value || '00:00:00')
   const [dbTime, setDbTime] = useState(new Date(0))
 
-  const convertStrToDate = function (clockStr) {
-    let [h, m, s] = clockStr.split(':')
+  // Time array --> Date object
+  const convertTimeArrToDateObj = function ([h, m, s]) {
     const seconds = Number(h) * 60 * 60 + Number(m) * 60 + Number (s)
-    setDbTime(new Date(seconds * 1000))
+    return new Date(seconds * 1000)
   }
-  
-  const addColons = function (cleanStr) {
-    const sixChars = ('000000' + cleanStr).slice(-6)
-    let output = ''
-    for (let i = 0; i < 6; i += 2) {
-      output += i == 2 || i == 4 ? ':' : ''
-      let twoChars = sixChars[i] + sixChars[i + 1]
-      output += twoChars
-    }
-    convertStrToDate(output)
-    setValue(output)
+  // Date object --> Time array
+  const convertDateObjToArr = function (dateObj) {
+    let h = dateObj.getUTCHours()
+    let m = dateObj.getUTCMinutes()
+    let s = dateObj.getUTCSeconds()
+    return [h, m, s]
   }
-  
-  const updateByStepValue = function (prev, sign) {
-    let [h, m, s] = value.split(':')
-    m = Number(m) + Number(sign)
-    h = Number(h)
-    if (m < 0) {
-      if (h !== 0) {
-        h -= 1
-        m = 59
-      } else {
-        m = 0
-      }
-    }
-    if (m > 59) {
-      h += 1
-      m = m % 60
-    }    
+
+  // Time array --> Time string
+  const convertTimeArrToStr = function ([h, m, s]) {
     let ss = ("0" + s).slice(-2);
     let mm = ("0" + m).slice(-2);
     let hh = ("0" + h).slice(-2);
     const timeStr = `${hh}:${mm}:${ss}`
-    convertStrToDate(timeStr)
+    return timeStr
+  }
+  // Time string --> Time array
+  const convertTimeStrToArr = function (rawStr) {
+    // Let h, m, s from rawStr
+    let s = Math.floor(rawStr % 100)
+    let m = Math.floor((rawStr / 100) % 100)
+    let h = Math.floor((rawStr / 100 / 100) % 100)
+    return [h, m, s]
+  }
+
+  // Validity check for user's raw input
+  const isAllNumbers = function (noColons) {
+    // If input is not all digits, revert to dbTime
+    const isnum = /^\d+$/.test(noColons)
+    if (!isnum) {
+      console.log('is no a numero');
+      return false
+    }
+    return true
+  }
+  
+  // Adjust time with butttons
+  const updateByStepValue = function (plusMinus) {
+    let newTime = convertTimeArrToDateObj(value.split(':'))
+    newTime.setUTCMinutes(newTime.getUTCMinutes() + plusMinus)
+    const timeArr = convertDateObjToArr(newTime)
+    setDbTime(convertTimeArrToDateObj(timeArr))
+    const timeStr = convertTimeArrToStr(timeArr)
     setValue(timeStr)
   }
 
+  // Adjust time by manually entering a new time
   const handleBlur = function (rawStr) {
-    const cleanStr = rawStr.split(':').join('')    
-    return addColons(cleanStr)
+    const noColons = rawStr.split(':').join('')
+    const isValid = isAllNumbers(noColons)
+    if (isValid) {
+      // noColons is a string of only numbers
+      const timeArr = convertTimeStrToArr(noColons)
+      // timeArr may include e.g. 90sec, convert to 1min30sec
+      const validTimeArr = convertDateObjToArr(convertTimeArrToDateObj(timeArr))
+      setDbTime(convertTimeArrToDateObj(validTimeArr))
+      const timeStr = convertTimeArrToStr(validTimeArr)
+      setValue(timeStr)
+    } else {
+      const timeStr = convertTimeArrToStr(convertDateObjToArr(dbTime))
+      setValue(timeStr)
+    }
   }
 
   return (
     <>
-      <i className="fa fa-chevron-up" onClick={e=>updateByStepValue(false, 1)}></i>
+      <i className="fa fa-chevron-up" onClick={e => updateByStepValue(1)}></i>
       <input
         value={value}
         onFocus={e => e.target.select()}
@@ -65,7 +87,7 @@ const StepInputTimer = function(props) {
         type='text'
         step={props.format === 'clock' ? 60 : 1}
       />
-      <i className="fa fa-chevron-down" onClick={e=>setValue(prev => updateByStepValue(prev, -1))}></i>
+      <i className="fa fa-chevron-down" onClick={e => updateByStepValue(-1)}></i>
     </>
   )
 }
