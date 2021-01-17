c1f70d61c72fe40599dd80b9fe72c586a8dc78bb
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-17 00:25:27 -0800

    WIP refactor stopwatch
    bring play-pause-save into stopwatch and attempt to simplify paths

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 import Button from './Button';
@@ -9,10 +9,15 @@ import StepInputClock from './StepInputClock'
 import './Stopwatch.scss'
 
 interface Data {
-  [key: string]: Date;
+  [key: string]: Date | number | null;
 }
 
-const data: Data = {};
+const data: Data = {
+  start_time: null,
+  end_time: null,
+  pause_start_time: null,
+  cumulative_pause_duration: 0
+};
 
 /* TODO
 - Render calendar as a layer on top of the rest of the page, so it doesn't move other elements around
@@ -20,44 +25,108 @@ const data: Data = {};
 - On date change, update y, m, d of start-time in state and db
 - Debug play/pause, sometimes it renders as a weird time with letters?
 - Change 'stop' to 'save', save the data and reset the component
-DONE Set clock start time to have max time of .now()
 */
 
 const Stopwatch = () => {
   const [description, setDescription] = useState('');
   const [calendarDate, setCalendarDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(false)
-  const [isActiveTimer, setIsActiveTimer] = useState(false)
-  const [initTimer, setInitTimer] = useState({
-    start: null,
-    pause: null,
-    stop: null,
-    action: null,
-  });
+  const [isTimerActive, setIsTimerActive] = useState(false)
+  const [timerVal, setTimerVal] = useState(0) // is a number of milliseconds
+  const [timerObj, setTimerObj] = useState({...data});
+
+
+  useEffect(() => {
+    if (isTimerActive) {
+      const timer = setInterval(() => {
+        setTimerVal(Number(new Date()) - Number(timerObj.start_time) - Number(timerObj.cumulative_pause_duration));
+      }, 1000);
+      
+      return () => clearInterval(timer);
+    }
+    if (timerObj.end_time) {
+      setTimerVal(0)
+    }
+  }, [timerObj, isTimerActive])
+
 
   const calendarState = (value: Date) => {
     console.log(value);
-    
     setCalendarDate(value)
     setShowCalendar(!showCalendar)
+    // Update task start time with new year, month, day
+  }
+
+  const calculateTime = (start: Date, end: Date) => {
+    return Number(end) - Number(start);
   }
 
   const timerAction = (timephase: string) => {
-    setIsActiveTimer(() => {
-      if (timephase === 'stop') return false
-      return !isActiveTimer;
+    setIsTimerActive(() => {
+      if (timephase === 'end_time') return false
+      return !isTimerActive;
     })
 
-    setInitTimer( (prev: any) => {
-      if (timephase !== 'start' || !data[timephase]) data[timephase] = new Date();
-      return {
-        ...prev,
-        stop: null,
-        [timephase]: data[timephase],
-        action: timephase,
-      };
-    });
+    // setTimerObj( (prev: any) => {
+    //   if (timephase !== 'start' || !data[timephase]) data[timephase] = new Date();
+    //   return {
+    //     ...prev,
+    //     end: null,
+    //     [timephase]: data[timephase],
+    //     action: timephase,
+    //   };
+    // });
+
+    switch (timephase) {
+      case 'PLAY':
+        setIsTimerActive(true);
+        if (timerObj.pause_start_time) {
+          const old_pause_dur = timerObj.cumulative_pause_duration
+          const new_pause_dur = Number(old_pause_dur) + Number(new Date()) - Number(timerObj.pause_start_time)
+          setTimerObj(prev => {
+            return {
+              ...prev, 
+              cumulative_pause_duration: new_pause_dur,
+              pause_start_time: null
+            };
+          })
+        } else {
+          setTimerObj(prev => {
+            return {
+              ...prev, 
+              start_time: new Date()
+            };
+          })
+        }
+        // setTimerVal(Number(new Date()) - Number(timerObj.start_time) - Number(timerObj.cumulative_pause_duration));
+        break;
+      case 'SAVE':
+        setIsTimerActive(false)
+        setTimerObj(prev => {
+          return {
+            ...prev, 
+            end_time: new Date()
+          };
+        })
+        // saveToDB
+        setTimerObj({...data})
+        console.log(timerObj)
+        break;
+      case 'PAUSE':
+        setIsTimerActive(false)
+        setTimerObj(prev => {
+          return {
+            ...prev, 
+            pause_start_time: new Date()
+          }
+        })
+        // setTimerVal(Number(timerObj.pause_start_time) - Number(timerObj.start_time))
+        break;
+    }
   }
+  
+  
+  
 
   return (
     <>
@@ -119,23 +188,26 @@ const Stopwatch = () => {
           /> 
         }
 
+        ---------   
+        {Math.floor(Number(timerVal) / 1000)}
+           ---------
         <Timer 
-          startTime = {initTimer}
+          timerObj={timerObj}
         />
 
-        {!isActiveTimer &&
-          <Button play onClick={(e: any) => timerAction("start")}>
+        {!isTimerActive &&
+          <Button play onClick={(e: any) => timerAction("PLAY")}>
             <i className="far fa-play-circle fa-lg"></i>
           </Button>
         }
 
-        {isActiveTimer &&
-          <Button pause onClick={(e: any) => timerAction("pause")}>
+        {isTimerActive &&
+          <Button pause onClick={(e: any) => timerAction("PAUSE")}>
             <i className="far fa-pause-circle fa-lg"></i>
           </Button>
         }
 
-        <Button stop onClick={(e: any) => timerAction("stop")}>
+        <Button stop onClick={(e: any) => timerAction("SAVE")}>
           <i className="far fa-save fa-lg"></i>
         </Button>
         
