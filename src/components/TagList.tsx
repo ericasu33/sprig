7f0f2a99144cb91614aac6e261c4c3690380d24c
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-16 20:14:09 -0800

    Add framework to remove Tags

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import Tag from './Tag';
 
 import './TagList.scss';
 
+interface tag {
+  [key: string]: any;
+}
+
 const TagList = (props: any) => {
+
+  const [tags, setTags] = useState<tag[]>([]);
 
   let tagListClass = classNames("tag-list");
 
-  const tags = props.tags.map((opts: any) => (
-    <Tag 
-      key={opts.id}
-      name={opts.name}
-      color={opts.color}
-    />
-  ));
+
+  useEffect(() => {
+    setTags([
+      { id: 1, name: "test", color: "#FFFFFF", },
+    ]);
+  }, []);
+  
+  const removeTag = (id: number) => {
+    setTags((prev: any) => {
+      return prev.filter((tag: tag) => tag.id !== id);
+    });
+  };
+
+  const tagList = tags.map((tag) => {
+    return (<Tag
+      key={tag.id}
+      name={tag.name}
+      color={tag.color}
+      onRemove={() => removeTag(tag.id)} 
+    />)
+  });
   
   return (
     <div className={tagListClass}>
-      {tags}
+      {tagList}
     </div>
   );
 };
