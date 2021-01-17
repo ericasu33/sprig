124de2dac7c6156bc98ebf24c67e20e1950145ec
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-16 20:16:43 -0800

    Add remove button to tags

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import classNames from 'classnames';
 import './Tag.scss';
+import Button from './Button';
 
 const Tag = (props: any) => {
   let tagClass = classNames("tag");
   return (
     <div className={tagClass}>
-      {props.name}
+      <span>{props.name}</span>
+      <Button
+        delete
+        onClick={props.onRemove}
+      >X</Button>
     </div>
   );
 };
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,2 @@
 /// <reference types="react-scripts" />
-<<<<<<< HEAD
 declare module 'classnames';
-=======
-declare module 'classnames';
->>>>>>> feature/component/Button
