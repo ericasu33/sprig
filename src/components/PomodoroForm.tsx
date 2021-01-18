af698acfdb61249ccd64579399b3c51644c156e2
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-17 16:59:36 -0800

    Add styles (incomplete) and more forms
    Add basic sound dropdown form.
    Use sound dropdown form as a fill-in for the Pomodoro name form.
    Add general styles for entire form.

--- src/components/PomodoroForm.tsx
+++ src/components/PomodoroForm.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './PomodoroForm.scss';
 import StepInputInt from './StepInputInt';
 import StepInputTimer from './StepInputTimer';
 
 const PomodoroForm = (props: any) => {
+  const [sounds, setSounds]: [Array<string | undefined>, Function] = useState([])
+
+  useEffect(() => {
+    setSounds([
+      "test1.mp3",
+      "test2.mp3",
+      "test3.mp3",
+    ]);
+  },[]);
+  const soundOptions = sounds.map((sound) => {
+    return (
+      <option
+        value={sound}
+      >
+        {sound}
+      </option>
+    );
+  });
+
   return (
-    <div>
+    <div className="pomodoro-form">
       <div>
         <label>Name</label>
-        
+        <select>
+          {soundOptions}
+          <option selected disabled value="new-pomodoro">New Pomodoro</option>
+        </select>
       </div>
       <div>
         <label>Work</label>
@@ -16,6 +39,18 @@ const PomodoroForm = (props: any) => {
       <div>
         <label>Short Break</label>
         <StepInputTimer></StepInputTimer>
+        <div>
+          <select>
+            <option selected disabled value="none">Start Break Sound</option>
+            <option value="none">none</option>
+            {soundOptions}
+          </select>
+          <select>
+            <option selected disabled value="none">End Break Sound</option>
+            <option value="none">none</option>
+            {soundOptions}
+          </select>
+        </div>
       </div>
       <div>
         <label>Repeats</label>
@@ -24,6 +59,30 @@ const PomodoroForm = (props: any) => {
       <div>
         <label>Long Break</label>
         <StepInputTimer></StepInputTimer>
+        <div>
+          <select>
+            <option selected disabled value="none">Start Long Break Sound</option>
+            <option value="none">none</option>
+            {soundOptions}
+          </select>
+          <select>
+            <option selected disabled value="none">End Long Break Sound</option>
+            <option value="none">none</option>
+            {soundOptions}
+          </select>
+        </div>
+      </div>
+      <div>
+        <label>Total Duration</label>
+        <StepInputTimer disabled/>
+      </div>
+      <div>
+        <label>Total Work</label>
+        <StepInputTimer disabled/>
+      </div>
+      <div>
+        <label>% Work</label>
+        <StepInputTimer disabled/>
       </div>
     </div>
   );
