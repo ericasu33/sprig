7a35922c6d89958d1b784c3cdcb44c09489b1e7c
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-19 11:29:35 -0800

    DELETE all non-StepInput components
    to simplify git operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import './App.scss';
-import Stopwatch from './components/Stopwatch';
-import PomodoroForm from './components/PomodoroForm'
 
 function App() {
   return (
     <div>
-      POMODORO
-      <br />
-      <PomodoroForm />
-      <br />
-      <br />
-      STOPWATCH
-      <br />
-      <Stopwatch />
     </div>
   );
 }
