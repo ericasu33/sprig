036417a86f2b6ae29ecdc9a68b0980a059e2c57a
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-20 12:06:38 -0800

    use Category data structure to render category
    minor update to styles so the dropdown floats above other elements

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,66 +2,73 @@ import React, { useState } from 'react';
 import { ChromePicker } from 'react-color';
 import classNames from 'classnames';
 import Button from './Button';
+import './Category.scss'
 
 const Category = (props: any) => {
   let categoryClass = classNames('category_default', {
     'category': props.color
-  })
+  })  
 
-  const [categoryName, setCategoryName] = useState(props.category_name || '')
+  const [categoryName, setCategoryName] = useState(props.category.name || '')
+  const [categoryColor, setCategoryColor] = useState(props.category.color || '#fff')
   const [category, setCategory] = useState(false)
   const [colour, setColour] = useState(false)
-  const [categoryColor, setCategoryColor] = useState({
-    background: '#ffffff'
-  })
 
   const handleChangeComplete = (color: any) => {
-    setCategoryColor({
-      background:color.hex
+    setCategoryColor(color.hex)
+  }
+
+  const updateCategory = () => {
+    props.updateCategory('category', {
+      ...props.category,
+      name: categoryName,
+      color: categoryColor
     })
   }
   
   return (
     <>
       {/* show */}
-      <div className={categoryClass}> {props.category_name} </div>
-      
-      <div onClick={(e) => setCategory(!category)}> 
-        <i className="far fa-plus-square"></i>
-        New category
-      </div>
+      <div className={categoryClass}> 
+        {props.category.name} 
+        
+        <div onClick={(e) => setCategory(!category)}> 
+          <i className="far fa-plus-square"></i>
+          New category
+        </div>
 
-      {/* create */}
-      {category &&
-        <div>
-          <input
-            value={categoryName}
-            onChange={(e) => setCategoryName(e.target.value)}
-            name='categoryName'
-            type='text'
-            form='form2'
-            placeholder="Category name"
-            size={50}
-          />
+        {/* create */}
+        {category &&
+          <div className='category-picker'>
+            <input
+              value={categoryName}
+              onChange={(e) => setCategoryName(e.target.value)}
+              name='categoryName'
+              type='text'
+              form='form2'
+              placeholder="Category name"
+              size={50}
+            />
 
-          <div>
-            <Button category_colour onClick={(e: any) => setColour(!colour)}>
-              Pick Color
+            <div>
+              <Button category_colour onClick={(e: any) => setColour(!colour)}>
+                Pick Color
+              </Button>
+              {colour &&
+                <div>
+                  <ChromePicker
+                    color={categoryColor}
+                    onChangeComplete={handleChangeComplete}
+                  />
+                </div>
+              }
+            </div>
+            <Button category_create onClick={(e: any) => updateCategory()}>
+              Create
             </Button>
-            {colour &&
-              <div>
-                <ChromePicker
-                  color={categoryColor.background}
-                  onChangeComplete={handleChangeComplete}
-                />
-              </div>
-            }
           </div>
-        <Button category_create>
-          Create
-        </Button>
+        }
       </div>
-      }
     </>
   );
 };
