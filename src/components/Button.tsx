14895ae36444887ed6fc70bfa0925b1d4b5faacb
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-19 22:34:30 -0800

    create category create & colour buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,8 @@ const Button = (props: any) => {
     'button--delete': props.delete,
     'button--pause': props.pause,
     'button--close': props.close,
+    'button--category_create': props.category_create,
+    'button--category_colour': props.category_colour
  });
   return (
     <button type="button" disabled={props.disabled} className={buttonClass} onClick={props.onClick}>
