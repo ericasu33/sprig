33fb92b535308c48d1699e63ec8e8198e7fea5a4
Erica <ericasu33@gmail.com>
2021-01-20 22:03:43 -0800

    create pie chart (total time duration by category) in percentage and total time duration displayed in hh:mm:ss on the top (to be moved to center when doing final css)

--- src/components/Pie.tsx
+++ src/components/Pie.tsx
@@ -1,5 +1,6 @@
 import { PieChart } from 'react-minimal-pie-chart';
-import { useAxiosGet } from '../Hooks/HTTPRequest'
+import { useAxiosGet } from '../Hooks/HTTPRequest';
+import Loader from './Loader';
 
 //calculate by category
 //based on total time
@@ -8,43 +9,79 @@ const PieEntry = ( props : any ) => {
   const url = "http://localhost:8080/api/stopwatches"
   const stopwatches : any = useAxiosGet(url)
   let content = null;
+  let sumOfValue: number = 0;
+
+  const totalTimeUsed = (seconds: number) => {
+    const hour = Math.floor(seconds % (3600 * 24) / 3600);
+    const minute = Math.floor(seconds % 3600 / 60);
+    const second = Math.floor(seconds % 60);
+
+    return `${hour} : ${minute} : ${second}`
+  }
+
+  if (stopwatches.error) {
+    content = 
+    <p>
+      There was an error, please refresh or try again later
+    </p>
+  }
+
+  if (stopwatches.loading) {
+    console.log("LOADDDDING")
+    content = <Loader />
+  }
 
   if (stopwatches.data) {
-    content = true;
-
-    const aggregateTotalDurationByCategory = ( entries : any ) => {
-    
-      const result : any  = {};
+    const filterStopwatchData = ( entries : any ) => {
+      const result : any = [];
 
       for (let entry of entries.data) {
+        const entryObj: any = {};
+
         for (const [key] of Object.entries(entry)) {
-          if (key === "category_name") {
-            result.name = entry.category_name
-          }
+            if (key === "category_name") {
+              entryObj.name = entry.category_name
+            }
 
-          if (key === "total_duration_ms") {
-            result.value = entry.total_duration_ms
-          }
+            if (key === "total_duration_ms") {
+              entryObj.value = entry.total_duration_ms
+            }
 
-          if (key === "category_color") {
-            result.color = `#${entry.category_color}`
-          }
+            if (key === "category_color") {
+              entryObj.color = `#${entry.category_color}`
+            }
         }
-
-        if (result.name) {
-          result.value = result.value + entry.total_duration_ms;
-        } else {
-          result.value = entry.total_duration_ms;
-        }
+        result.push(entryObj);
       }
-
       return result
     } 
-  
-    console.log(aggregateTotalDurationByCategory(stopwatches))
-    const pieChartData = [aggregateTotalDurationByCategory(stopwatches)];
-    console.log(pieChartData) 
-  } 
+
+    const aggregateTotalDurationByCategory = ( filteredEntries : any ) =>  {
+      const entryObj : any = {};
+      const result : any = [];
+      
+
+      for (const entry of filteredEntries) {
+        const nameColor = entry.name + ',' + entry.color
+         if (entryObj[nameColor]) {
+            entryObj[nameColor] = entryObj[nameColor] + entry.value;
+            sumOfValue += entry.value;
+          } else {
+            entryObj[nameColor] = entry.value
+            sumOfValue += entry.value;
+          }
+        }
+
+        for (const entry in entryObj) {
+          const nameColorArr = entry.split(",")
+          result.push({
+            name: nameColorArr[0],
+            value: Math.floor(entryObj[entry]/sumOfValue * 100),
+            color: nameColorArr[1]
+          })
+        }
+        return result;
+      }
 
   const defaultLabelStyle = {
     fontSize: '5px',
@@ -52,15 +89,12 @@ const PieEntry = ( props : any ) => {
     fill: '#FFF'
   };
 
-  const chartData = pieChartData
+    const chartData = aggregateTotalDurationByCategory(filterStopwatchData(stopwatches));
     
   const shiftSize = 7;
   const lineWidth = 60;
   
-
-    return (
-      <>
-      {content && 
+    content = 
         <PieChart
           data = {chartData}
           style= {{
@@ -77,10 +111,14 @@ const PieEntry = ( props : any ) => {
             ...defaultLabelStyle,
           }}
         />
-        }
-      </>
-    )
-  
+  }
+
+  return (
+    <div>
+      {totalTimeUsed(sumOfValue)}
+      {content}
+    </div>
+  )
 }
 
 
