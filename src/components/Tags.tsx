536b562c24ec841637a7a48b29d190d62ffd3d73
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-20 18:35:10 -0800

    WIP test CreatableSelect

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,30 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CreatableSelect from 'react-select/creatable';
+// import { colourOptions } from '../data';
 
 const components = {
   DropdownIndicator: null,
+  ClearIndicator: null
 };
 
 const createOption = (label: string) => ({
   label,
-  value: label,
+  value: 'hurdurr',
 });
 
+interface Tag {
+  id: number | null,
+  name: string | null,
+  color: string | null
+}
+
 const CreatableInputOnly = (props: any) => {
-
-  const [value, setValue] = useState([{label: 'tag', value: 'firsttaggo'}])
+  
+  
+  // const [value, setValue] = useState([{label: 'tag', value: 'tag'}])
+  // [{id: 0, name: 'food', color: '#ee0'}, {id: 1, name: 'dessert', color: '#e0e'}],
+  const [value, setValue] = useState(props.tags)
   const [inputValue, setInputValue] = useState('')
-
+  
+  // useEffect(() => {
+  //   // const tagNames: (string[] | null[] | undefined) = []
+  //   props.tags.map((tag: Tag) => {
+  //     value.push(tag.name)
+  //   })
+  //   setValue(tagNames)
+  // }, props.tags)
+  
   const handleChange = (value: any, actionMeta: any) => {
     console.group('Value Changed');
     console.log(value);
     console.log(`action: ${actionMeta.action}`);
     console.groupEnd();
-    setValue(value);
   };
+
   const handleInputChange = (inputValue: string) => {
     setInputValue(inputValue);
   };
+
   const handleKeyDown = (event: any) => {
     if (!inputValue) return;
     switch (event.key) {
@@ -33,31 +53,37 @@ const CreatableInputOnly = (props: any) => {
         console.group('Value Added');
         console.log(value);
         console.groupEnd();
-        setValue(prev => {
-          return [
-            ...prev, 
-            createOption(inputValue)
-          ]
-        })
+        props.onChange('tags', {id: null, name: inputValue, color: null})
         setInputValue('');
         event.preventDefault();
     }
   };
+
   return (
     <CreatableSelect
       className='tags' 
-      components={components}
-      inputValue={inputValue}
       isClearable
       isMulti
-      menuIsOpen={false}
       onChange={handleChange}
-      onInputChange={handleInputChange}
-      onKeyDown={handleKeyDown}
       placeholder="Type something and press enter..."
-      value={value}
+      options={value}
     />
   );
+  // return (
+  //   <CreatableSelect
+  //     className='tags' 
+  //     components={components}
+  //     inputValue={inputValue}
+  //     isClearable
+  //     isMulti
+  //     menuIsOpen={false}
+  //     onChange={handleChange}
+  //     onInputChange={handleInputChange}
+  //     onKeyDown={handleKeyDown}
+  //     placeholder="Type something and press enter..."
+  //     value={value}
+  //   />
+  // );
 }
 
 export default CreatableInputOnly;
