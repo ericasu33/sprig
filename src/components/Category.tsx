fb466a96d69a5e15bf49912475a8e3b3f6f9a29e
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-20 21:24:18 -0800

    WIP update Category to use react-select
    todo:
    - use categories array from props
    - update parent state on change

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,76 +1,44 @@
-import React, { useState } from 'react';
-import { ChromePicker } from 'react-color';
-import classNames from 'classnames';
-import Button from './Button';
-import './Category.scss'
+import React, { useState, useEffect } from 'react';
+
+import CreatableSelect from 'react-select/creatable';
+
+const createTag = (label: string) => ({
+  id: null,
+  label,
+  value: label.toLowerCase().replace(/\W/g, ''),
+});
+
+const defaultOptions = [
+  createTag('One'),
+  createTag('Two'),
+  createTag('Three'),
+];
 
 const Category = (props: any) => {
-  let categoryClass = classNames('category_default', {
-    'category': props.color
-  })  
 
-  const [categoryName, setCategoryName] = useState(props.category.name || '')
-  const [categoryColor, setCategoryColor] = useState(props.category.color || '#fff')
-  const [category, setCategory] = useState(false)
-  const [colour, setColour] = useState(false)
-
-  const handleChangeComplete = (color: any) => {
-    setCategoryColor(color.hex)
-  }
-
-  const updateCategory = () => {
-    props.updateCategory('category', {
-      ...props.category,
-      name: categoryName,
-      color: categoryColor
-    })
-  }
+  const [options, setOptions] = useState(defaultOptions)
+  const [value, setValue] = useState(createTag(''))
   
+  const handleChange = (newValue: any) => {
+    setValue(newValue);
+  };
+
+  const handleCreate = (inputValue: any) => {
+    const newOption = createTag(inputValue);
+    console.log(newOption);
+    setOptions(prev => [...options, newOption])
+  };
+
   return (
-    <>
-      {/* show */}
-      <div className={categoryClass}> 
-        {props.category.name} 
-        
-        <div onClick={(e) => setCategory(!category)}> 
-          <i className="far fa-plus-square"></i>
-          New category
-        </div>
-
-        {/* create */}
-        {category &&
-          <div className='category-picker'>
-            <input
-              value={categoryName}
-              onChange={(e) => setCategoryName(e.target.value)}
-              name='categoryName'
-              type='text'
-              form='form2'
-              placeholder="Category name"
-              size={50}
-            />
-
-            <div>
-              <Button category_colour onClick={(e: any) => setColour(!colour)}>
-                Pick Color
-              </Button>
-              {colour &&
-                <div>
-                  <ChromePicker
-                    color={categoryColor}
-                    onChangeComplete={handleChangeComplete}
-                  />
-                </div>
-              }
-            </div>
-            <Button category_create onClick={(e: any) => updateCategory()}>
-              Create
-            </Button>
-          </div>
-        }
-      </div>
-    </>
+    <CreatableSelect
+      isClearable
+      onChange={handleChange}
+      onCreateOption={handleCreate}
+      options={options}
+      value={value}
+    />
   );
-};
+}
+
 
 export default Category;
