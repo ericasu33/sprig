4d1e5f9882359793b2b75f96c01a03f46dda96f7
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-23 21:29:36 -0800

    move timer presets to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -6,50 +6,6 @@ import PomodoroTimer from 'components/Pomodoro/PomodoroTimer';
 import 'components/Pomodoro/PomodoroTimer'
 import presetPomodoros from 'db/preset-timers'
 
-import { ISound, ITimer } from './ts-interfaces/interfaces';
-
-const timerData = [
-  {
-    id: 1,
-    uid: 0,
-    name: "Classic Pomodoro",
-    cycles: 2,
-    work: 25 * 60,
-    short_break: 5 * 60,
-    long_break: 35 * 60,
-    short_b_start_sound: 0,
-    short_b_end_sound: 0,
-    long_b_start_sound: 0,
-    long_b_end_sound: 0,
-  },
-  {
-    id: 2,
-    uid: 0,
-    name: "Classic Eye-Strain",
-    cycles: 3,
-    work: 2,
-    short_break: 2,
-    long_break: 2,
-    short_b_start_sound: 0,
-    short_b_end_sound: 0,
-    long_b_start_sound: 0,
-    long_b_end_sound: 0,
-  },
-  {
-    id: 3,
-    uid: 0,
-    name: "Classic 50-7",
-    cycles: 3,
-    work: 50 * 60,
-    short_break: 7 * 60,
-    long_break: 0,
-    short_b_start_sound: 0,
-    short_b_end_sound: 0,
-    long_b_start_sound: 0,
-    long_b_end_sound: 0,
-  },
-  
-];
 import { ISound, ITimer } from 'ts-interfaces/interfaces';
 
 const sounds: ISound[] = [
@@ -59,7 +15,7 @@ const sounds: ISound[] = [
 ];
 
 function App() {
-  const [timerPresets, setTimerPresets] = useState(timerData);
+  const [timerPresets, setTimerPresets] = useState(presetPomodoros);
   const [soundFiles, setSoundFiles] = useState(sounds);
 
   const handleAddTimer = (timer: ITimer) => {
@@ -97,7 +53,7 @@ function App() {
           <StopwatchActive />
         </section>
         <section className='section-sw-entries'>
-          <StopwatchList />
+          <AnalyzeStopwatch />
         </section>
       </section>
     </main>
---
+++ src/db/preset-timers.tsx
@@ -0,0 +1,45 @@
+
+const presetPomodoros = [
+  {
+    id: 1,
+    uid: 0,
+    name: "Classic Pomodoro",
+    cycles: 2,
+    work: 25 * 60,
+    short_break: 5 * 60,
+    long_break: 35 * 60,
+    short_b_start_sound: 0,
+    short_b_end_sound: 0,
+    long_b_start_sound: 0,
+    long_b_end_sound: 0,
+  },
+  {
+    id: 2,
+    uid: 0,
+    name: "Classic Eye-Strain",
+    cycles: 3,
+    work: 2,
+    short_break: 2,
+    long_break: 2,
+    short_b_start_sound: 0,
+    short_b_end_sound: 0,
+    long_b_start_sound: 0,
+    long_b_end_sound: 0,
+  },
+  {
+    id: 3,
+    uid: 0,
+    name: "Classic 50-7",
+    cycles: 3,
+    work: 50 * 60,
+    short_break: 7 * 60,
+    long_break: 0,
+    short_b_start_sound: 0,
+    short_b_end_sound: 0,
+    long_b_start_sound: 0,
+    long_b_end_sound: 0,
+  },
+  
+];
+
+export default presetPomodoros;
