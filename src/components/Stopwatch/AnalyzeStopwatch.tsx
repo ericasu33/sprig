583cddf9e9ca25008f14de3811b4439f2a75ff0f
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-23 21:30:21 -0800

    create analyzeStopwatch component for data & charts

---
+++ src/components/Stopwatch/AnalyzeStopwatch.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import StopwatchList from './StopwatchList'
+// import Charts from './Charts'
+
+
+const AnalyzeStopwatch = (props: any) => {
+
+
+  return (
+    <div>
+      <section className='section-sw-entries'>
+        <StopwatchList />
+      </section>
+    </div>
+  )
+}
+
+export default AnalyzeStopwatch;
