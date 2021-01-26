8408474ba5842932fecd06133231a0af8a85fe5b
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-25 22:04:32 -0800

    Refactor ProgresBarByCategory

--- src/components/Stopwatch/ProgressBarByCategory.tsx
+++ src/components/Stopwatch/ProgressBarByCategory.tsx
@@ -9,53 +9,36 @@ import { filterStopwatchData } from '../../helpers/displayStopwatchByCatData'
 const Bar = ( props : any ) => {
   const stopwatches : any = props.entries;
   let content = null;
-  let sumOfValue: number = 0;
 
-  if (!stopwatches) {
-    content = <Loader />
+  const aggregateTotalDurationByCategory = ( filteredEntries : any ) =>  {
+    const entryObj : any = {};
+    const results : any = [];
+    let sum = 0;
+    for (const entry of filteredEntries) {
+      if (entryObj[entry.category.id]) {
+        entryObj[entry.category.id].value += entry.end_time - entry.start_time - entry.cumulative_pause_duration
+      } else {
+        entryObj[entry.category.id] = {
+          title: entry.category.value,
+          value: entry.end_time - entry.start_time - entry.cumulative_pause_duration,
+          color: entry.category.color,
+        };
+      }
+      sum += entry.end_time - entry.start_time - entry.cumulative_pause_duration;
+    }
+    for (const id in entryObj) {
+      results.push({
+        ...entryObj[id],
+        value: Number((entryObj[id].value/sum * 100).toFixed(2)),
+      });
+    }
+    return results;
   }
 
   if (stopwatches) {
+    const dataArray : any = aggregateTotalDurationByCategory(stopwatches);
 
-    const aggregateTotalDurationByCategory = (filteredEntries: any) => {
-      const entryObj: any = {};
-      let result: any = [];
-
-      for (const entry of filteredEntries) {
-        const nameColor = entry.name + ',' + entry.color
-        if (entryObj[nameColor]) {
-          entryObj[nameColor] = entryObj[nameColor] + entry.value;
-          sumOfValue += entry.value;
-        } else {
-          entryObj[nameColor] = entry.value
-          sumOfValue += entry.value;
-        }
-      }
-
-      for (const entry in entryObj) {
-        const nameColorArr = entry.split(",")
-        result.push({
-          name: nameColorArr[0],
-          valueInSec: entryObj[entry],
-          value: +((entryObj[entry] / sumOfValue * 100).toFixed(2)),
-          color: nameColorArr[1]
-        })
-      }
-
-      if (result.length === 0) {
-        result = [{
-          color: "#A8A8A8",
-          name: "No Category",
-          value: 0,
-          valueInSec: 0
-        }]
-      }
-      
-      console.log(result)
-      return result;
-    }
     
-  const dataArray : any = aggregateTotalDurationByCategory(filterStopwatchData(stopwatches));
 
     content = dataArray.map((entry : any, key : any) => {
       return (
@@ -68,9 +51,7 @@ const Bar = ( props : any ) => {
           completed={entry.value}
         />
       )
-    }
-    )
-        
+    });
   }
 
   return (
