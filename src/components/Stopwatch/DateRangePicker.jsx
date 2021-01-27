350db7c3116fa98a55eb8b7c162438190a7b48e7
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-26 22:33:34 -0800

    Fix use of dates to use current dates

--- src/components/Stopwatch/DateRangePicker.jsx
+++ src/components/Stopwatch/DateRangePicker.jsx
@@ -4,17 +4,12 @@ import Button from '../Button'
 import './DateRangePicker.scss'
 
 export default function DateRange(props) {
-  const now = new Date();
-  const sevenDaysAgo = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
-  const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
-  const oneYearAgo = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
-  const lastSunday = new Date(now.setDate(now.getDate() - now.getDay()));
-  const todayEnd = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
 
   const [value, setValue] = useState();
   const {onChange} = props;
   
   const dateIntervalChange = (value, operator = "") => {
+    const now = new Date();
     const diffInMs = value[1] - value[0]
     const oneDay = 1000 * 60 * 60 * 24;
     const diffInDays = Math.round(diffInMs / oneDay)
@@ -30,6 +25,15 @@ export default function DateRange(props) {
     } else if (operator === "subtract") {
       start.setDate(start.getDate() - diffInDays);
       end.setDate(end.getDate() - diffInDays)
+    } else if (operator === "week") {
+      start = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
+      end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
+    } else if (operator === "month") {
+      start = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+      end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
+    } else if (operator === "year") {
+      start = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
+      end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
     }
     setValue([start, end]);
     onChange("date_range",[start, end]);
@@ -50,9 +54,9 @@ export default function DateRange(props) {
 
       <Button date_range_right onClick={e => dateIntervalChange(value, 'add')} />
       <Button date_range_reset onClick={() => dateIntervalChange([null,null])}>Clear</Button>
-      <Button date_range_reset onClick={() => dateIntervalChange([sevenDaysAgo, todayEnd])}>Past week</Button>
-      <Button date_range_reset onClick={() => dateIntervalChange([oneMonthAgo, todayEnd])}>Past month</Button>
-      <Button date_range_reset onClick={() => dateIntervalChange([oneYearAgo, todayEnd])}>Past year</Button>
+      <Button date_range_reset onClick={() => dateIntervalChange(value, 'week')}>Past week</Button>
+      <Button date_range_reset onClick={() => dateIntervalChange(value, 'month')}>Past month</Button>
+      <Button date_range_reset onClick={() => dateIntervalChange(value, 'year')}>Past year</Button>
 
     </div>
   );
