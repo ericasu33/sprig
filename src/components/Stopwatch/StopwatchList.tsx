60ad36decfa3ad4e326880f2ff304c2449009ca5
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-28 09:25:25 -0800

    hardcode headers for entries

--- src/components/Stopwatch/StopwatchList.tsx
+++ src/components/Stopwatch/StopwatchList.tsx
@@ -22,6 +22,22 @@ const StopwatchList = (props: any) => {
   return (
     <>
       <section className="sw-entries">
+        
+        <div className='sw-header'>
+          <div className='sw-header-category'>Categories</div>
+          <div className='sw-header-tags'>Tags</div>
+          <div className='sw-header-times'>
+            <div className='sw-header-times-start'>Start</div>
+            <div className='sw-header-times-end'>End</div>
+            <div className='sw-header-times-intensity'>Intensity</div>
+          </div>
+          <div className='sw-header-calcs'>
+            <div className='sw-header-calcs-total'>Total</div>
+            <div className='sw-header-calcs-effective'>Effective</div>
+          </div>
+          <div className='sw-header-buttons'></div>
+        </div>
+
         <ul className="sw-entries-list">{ entriesList }</ul>
       </section>
     </>
